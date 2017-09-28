ece979f3fa950ec9fc50a530e3862e4df62ae16d
Isaac Khor <s.github@isaackhor.com>
2017-09-28 21:11:29 +0800

    Guarantee js loading order

--- scripts/background.js
+++ scripts/background.js
@@ -1,17 +1,17 @@
 function toggleReadingMode(tab) {
   // The tabs api default to current tab if id is not provided
-  var tabid = tab ? tab.id : null;
-  browser.tabs.executeScript(tabid, {
-    allFrames: false,
-    file: "/libs/Readability.js",
-    runAt: "document_idle"
-  });
-  browser.tabs.executeScript(tabid, {
-    allFrames: false,
-    file: "/scripts/content.js",
-    runAt: "document_idle"
-  });
-  console.log("hello i am pressed thingy yay!");
+  var tabid = tab ? tab.id : null
+
+  // Guarantee order of script loading
+  var readabilityPr = browser.tabs.executeScript(tabid, { file: "/libs/Readability.js" })
+  readabilityPr.then(
+    function() {
+      browser.tabs.executeScript(tabid, { file: "/scripts/content.js" })
+    },
+    function() {
+      console.log("Readability failed to load")
+    })
+  console.log("Toggle reading mode")
 }
 
 function processCommand(cmd) {
