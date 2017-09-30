8f87898cd9b5360e5a4fc94435e397986e7acf7b
Isaac Khor <s.github@isaackhor.com>
2017-09-30 14:07:28 +0800

    Create basic options page

---
+++ scripts/options.js
@@ -0,0 +1,7 @@
+var stylesheetEditor = CodeMirror.fromTextArea(
+  document.getElementById("edit-stylesheet"),
+  { lineNumbers: true })
+
+var listEditor = CodeMirror.fromTextArea(
+  document.getElementById("edit-autolist"),
+  { lineNumbers: true })
