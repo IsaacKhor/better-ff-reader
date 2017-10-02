82aa695867f12d257c287d9b9c7754d61e1b3fbd
Isaac Khor <s.github@isaackhor.com>
2017-10-02 09:28:29 +0800

    Implement options page js

--- scripts/options.js
+++ scripts/options.js
@@ -1,7 +1,18 @@
-var stylesheetEditor = CodeMirror.fromTextArea(
-  document.getElementById("edit-stylesheet"),
-  { lineNumbers: true })
+function save() {
+  browser.storage.sync.set({
+    enableRegexes: listEditor.getValue().split('\n'),
+    stylesheet: stylesheetEditor.getValue()
+  })
+}
 
-var listEditor = CodeMirror.fromTextArea(
-  document.getElementById("edit-autolist"),
-  { lineNumbers: true })
+(function() {
+  var stylesheetEditor = CodeMirror.fromTextArea(
+    document.getElementById("edit-stylesheet"),
+    { lineNumbers: true })
+
+  var listEditor = CodeMirror.fromTextArea(
+    document.getElementById("edit-autolist"),
+    { lineNumbers: true })
+
+  document.getElementById("btn-save").addEventlistener('click', save())
+})()
